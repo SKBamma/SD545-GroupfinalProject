87346a46d13da3e3a985dec3ab8ce3c0d2cc8f3d
Rojin Bijukchhe <ro_jin25@icloud.com>
2024-04-22 11:51:41 -0500

    V 2.0.6 Home Page updated. Happy Coding. :)

--- music-app/src/pages/playlist/Music-player/music.player.tsx
+++ music-app/src/pages/playlist/Music-player/music.player.tsx
@@ -4,13 +4,20 @@ import "react-h5-audio-player/lib/styles.css";
 import "./music.player.css";
 
 export default function MusicPlayer() {
+  function handleNext() {
+    console.log("next");
+  }
+
   return (
     <div>
       <table className="table-playing">
         <tr className="tr-headers-playing">
           <td>
             <AudioPlayer
+              // header={currentSong?.title}
               src="http://localhost:4000/music/mocking_bird.mp3"
+              onClickNext={handleNext}
+              // onClickPrevious={handlePrevious}
 
               // other props here
             />
